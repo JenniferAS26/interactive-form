6f4ba9835b0ea893c6ab4bd240ff142940a03f19
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-07-05 15:48:53 -0500

    localStorage

---
+++ index.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('.signup');
+const users = [];
+const getData = () => {
+    const name = document.querySelector('#input-name').value;
+    const lastname = document.querySelector('#input-lastname').value;
+    const email = document.querySelector('#input-email').value;
+    const password = document.querySelector('#input-password').value;
+
+    let data = {name, lastname, email, password};
+    users.push(data);
+    localStorage.setItem('users', JSON.stringify(users));
+    
+
+};
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    getData();
+    const dataJS = JSON.parse(localStorage.getItem('users'));
+    dataJS.forEach(data => {
+        console.log(data);
+    });
+});
+
